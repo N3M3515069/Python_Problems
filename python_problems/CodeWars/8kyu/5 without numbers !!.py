d38af52d09a1823322c519fb1384bec0bff86f21
Kaustav Biswas <140887068+N3M3515069@users.noreply.github.com>
2023-08-13 16:14:13 +0530

    Add files via upload

---
+++ python_problems/CodeWars/8kyu/5 without numbers !!.py
@@ -0,0 +1,19 @@
+'''DESCRIPTION:
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)'''
+
+def unusual_five():
+    counter = []
+    val = str(ord('i'))
+
+    for i in val:
+        counter.append(i)
+    value = list(reversed(counter))
+    valuex = value.pop()
+    valuex = value.pop()
+    valuex = value.pop()
+    for i in valuex:
+        return int(i)
