96f70eabd54ec6a00b234f21619bbc7dee2b16f5
Kaustav Biswas <140887068+N3M3515069@users.noreply.github.com>
2023-08-15 11:04:22 +0530

    Add files via upload

---
+++ python_problems/CodeWars/7kyu/Shortest Word.py
@@ -0,0 +1,11 @@
+'''Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. '''
+
+
+def find_short(s):
+    counter = []
+    val = s.split()
+    for i in val:
+        counter.append(len(i))
+    return min(counter)
